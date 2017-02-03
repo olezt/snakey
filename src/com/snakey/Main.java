6bfbf6ae04f731ee4ab24bcbc0986f6509a9ce27
olezt <lfants666@gmail.com>
2017-02-03 12:11:28 +0200

    Center window position

--- src/com/snakey/Main.java
+++ src/com/snakey/Main.java
@@ -1,5 +1,8 @@
 package com.snakey;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class Main {
@@ -13,13 +16,16 @@ public class Main {
                 multiplayer = true;
         	}
         }
-		
+        // Get screen dimensions
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        
 		//Creating the window with all its awesome snaky features
 		Window f1= new Window(multiplayer);
 		
 		//Setting up the window settings
 		f1.setTitle("Snake");
 		f1.setSize(300,300);
+		f1.setLocation(dim.width/2-f1.getSize().width/2, dim.height/2-f1.getSize().height/2);
 		f1.setVisible(true);
 		f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
 
